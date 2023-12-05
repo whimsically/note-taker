32405e6fd07987294facecbdf82247df86d478f9
Amanda Norris <amandakathleennorris@gmail.com>
2023-12-05 15:04:37 -0600

    added /api GET

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,24 @@
-const express = require('express');
-
-const app = express();
+const api = require('express').Router();
+const fs = require('fs');
 
 //api GET
+//reads db.json
+api.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+            res.json(JSON.parse(data))
+        }
+    })
+
+});
 
 //api POST
+api.post('/', (req, res) => {
+
+});
 
 //api DELETE
 
-module.exports = app;
+module.exports = api;
