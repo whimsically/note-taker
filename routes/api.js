38261676c42e95899f784307434c453b5eb2dc11
Amanda Norris <amandakathleennorris@gmail.com>
2023-12-05 14:34:49 -0600

    added server.js routes, api.js"

---
+++ routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+//api GET
+
+//api POST
+
+//api DELETE
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,29 @@
+//requiring express + path packages
+const express = require('express');
+const path = require('path');
+// API route
+const api = require('./routes/api.js');
+
+//port number
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+//index page
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// /notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
